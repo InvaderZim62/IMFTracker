31da2c565a596197d27f5fc2c0dfe535e5555bf5
Invader Zim <pwstern@gmail.com>
2021-03-17 01:10:51 -0700

    Hid status bar and made LaunchScreen background black.

--- IMFTracker/ViewController.swift
+++ IMFTracker/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {  // also added "Status bar is initially hidden" = YES to Info.plist to hide during launch
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
